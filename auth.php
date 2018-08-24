4991c020698eb85ed34e59dc72eb310df71aa92a
zhen <zhenia.carnakova@yandex.ru>
2018-08-25 01:28:55 +0800

    dashboard

---
+++ auth.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Вход</title>
+    <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.css">
+  </head>
+
+  <body style="background:#9370DB">
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-md-auto">
+          <div style = "margin-top: 80px; border-radius: 20px;" class="card">
+            <div class="card-body"> 
+              <div><img style="width:300px;height:65px;margin:-5px 0px 5px -8px;" src="img/1.png"></div>
+              <div><img style="width:40px;height:40px;margin:10px 0 10px 0;"src="img/user-1633249_1280.png"></div>
+              <form action="">
+                <div class="form-group">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите логин" name="login">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль" name="password">
+                </div>
+                <div class="form-group form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div> 
+                <button type="submit" class="btn btn-primary">ВОЙТИ</button> 
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="/lib/jquery/jquery.js"></script>
+    <script src="/lib/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
